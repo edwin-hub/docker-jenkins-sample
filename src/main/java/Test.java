5bd5788348535c5066bcfcad76d3f89792873dfb
edwin-hub <54796282+edwin-hub@users.noreply.github.com>
2021-02-09 03:23:12 +0100

    Update Test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello tout le monde !");
         System.out.println("Hello tout le monde !");
+        System.out.println("Hello tout le monde !");
     }
 }
